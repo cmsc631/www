6fc24efe27e5bcc00d54d571ce4af0cabbd321e1
David Van Horn <dvanhorn@cs.umd.edu>
2015-08-25 13:38:34 -0400

    ML type infer examples.

---
+++ code/easy.ml
@@ -0,0 +1,17 @@
+let a z=z in
+let b=(a,a) in
+let c=(b,b) in
+let d=(c,c) in
+let e=(d,d) in
+let f=(e,e) in
+let g=(f,f) in
+let h=(g,g) in
+let i=(h,h) in
+let j=(i,i) in
+let k=(j,j) in
+let l=(k,k) in
+let m=(l,l) in
+let n=(m,m) in
+let o=(n,n) in
+  o
+
