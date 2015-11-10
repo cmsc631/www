df1f7df3775315eab70a177a82399fe9bb9f69ab
David Van Horn <dvanhorn@cs.umd.edu>
2015-11-10 15:17:39 -0500

    Blog code from last time.

---
+++ today/cont.ml
@@ -0,0 +1,87 @@
+type exp = Var of string
+	 | App of exp * exp
+	 | Fun of string * exp
+
+type env = (string * cls) list
+ and cls = Close of exp * env
+	 | App of cls * cls
+
+type c = CHole
+       | CAppL of c * cls
+       | CAppR of cls * c
+
+type k = KHole
+       | KAppL of k * cls
+       | KAppR of cls * k
+
+let rec plug_c (cxt : c) (x : cls) : cls =
+  match cxt with
+  | CHole -> x
+  | CAppL (cxt, cls) ->
+     App (plug_c cxt x, cls)
+  | CAppR (cls, cxt) ->
+     App (cls, plug_c cxt x)
+
+let rec id (xs : int list) : int list =
+  match xs with
+  | [] -> []
+  | x :: xs -> x :: (id xs)
+
+let rec rev_acc (xs : int list) (acc : int list) : int list =
+  match xs with
+  | [] -> acc
+  | x :: xs -> rev_acc xs (x :: acc)
+
+let rec rev (xs : int list) : int list =
+  rev_acc xs []
+  
+let rec decompose_c (x : cls) : (c * cls) option =
+  match x with
+  | App (Close (Fun (z, e), r), Close (Fun (y, e'), r')) ->
+     Some (CHole, x)
+  | Close (Var y, r) ->
+     Some (CHole, x)
+  | Close (App (e1, e2), r) ->
+     Some (CHole, x)
+
+  | App (c1, c2) ->
+     (match decompose_c c1 with
+      | None -> (match decompose_c c2 with
+		 | None -> None
+		 | Some (cxt, cls) ->
+		    Some (CAppR (c1, cxt), cls))
+      | Some (cxt, cls) ->
+	 Some (CAppL (cxt, c2), cls))
+
+  | Close (Fun (y, e), r) ->
+     None
+
+let rec decompose_k (x : cls) : (k * cls) option =
+  decompose_k_acc x KHole
+
+and decompose_k_acc (x : cls) (acc : k) : (k * cls) option =
+  match x with
+  | App (Close (Fun (z, e), r), Close (Fun (y, e'), r')) ->
+     Some (acc, x)
+  | Close (Var y, r) ->
+     Some (acc, x)
+  | Close (App (e1, e2), r) ->
+     Some (acc, x)
+  
+  | App (c1, c2) ->
+     (match decompose_k_acc c1 (KAppL (acc, c2)) with
+      | None -> (match decompose_k_acc c2 (KAppR (c1, acc)) with
+		 | None -> None
+		 | Some (cont, cls) as r -> r)
+      | Some (cont, cls) as r -> r)		    
+  | Close (Fun (y, e), r) ->
+     None
+	       
+let rec plug_k (cont : k) (x : cls) : cls =
+  match cont with
+  | KHole -> x
+  | KAppL (cont, cls) ->
+     plug_k cont (App (x, cls))
+  | KAppR (cls, cont) ->
+     plug_k cont (App (cls, x))
+
